96de010391bf0ddc85384bbc2ed1814c7b4ce8f3
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-04-12 10:20:23 +0200

    setup

---
+++ src/test/java/edu/exam/examexercise/ExamExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.exam.examexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
